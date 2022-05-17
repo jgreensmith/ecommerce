470a7a6826f1a3e196330e2dd3085181e37aff61
James Greensmith <jgreensmith13@gmail.com>
2022-05-17 23:54:56 +0100

    page slug

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -2,10 +2,10 @@ import React from 'react'
 import Layout from '../../components/Layout';
 import { client } from '../../lib/client';
 
-const Product = ({ title, image, name, price, details }) => {
-  console.log({ title, image, name, price, details })
+const Product = ({ image, name, price, details }) => {
+  console.log({ image, name, price, details })
   return (
-    <Layout title={title}>
+    <Layout title={name}>
       <h1>Butt </h1>
     </Layout>
   )
@@ -15,7 +15,6 @@ export const getServerSideProps = async (pageContext) => {
   const pageSlug = pageContext.query.slug;
 
   const query = `*[ _type == "product" && slug.current == $pageSlug ][0] {
-    title,
     image,
     name,
     price,
@@ -31,7 +30,6 @@ export const getServerSideProps = async (pageContext) => {
   } else {
     return {
       props: {
-        title: product.title,
         image: product.image,
         name: product.name,
         price: product.price,
