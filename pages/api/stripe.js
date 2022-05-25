090a7beb9b65a4bf243810b57d2ff78fdb11719b
James Greensmith <jgreensmith13@gmail.com>
2022-05-25 15:03:26 +0100

    stripe lib and api

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,21 +1,36 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+import { countryCodesArray } from "../../utils/countryCodes";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
+        const params = {
+            submit_type: "pay",
+            mode: "payment",
+            payment_method_types: ["card"],
+            billing_address_collection: "auto",
+            shipping_address_collection: {
+              allowed_countries: countryCodesArray,
+            },
+            shipping_options: [
+              { shipping_rate: "shr_1L3KQkJQzZCeROhUPCLHPGGZ" },
+              { shipping_rate: "shr_1L3KSvJQzZCeROhUPWnMm2wN" },
+            ],
+            line_items: [
+                {
+                  // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                  price: '{{PRICE_ID}}',
+                  quantity: 1,
+                },
+              ],
+              mode: 'payment',
+              success_url: `${req.headers.origin}/?success=true`,
+              cancel_url: `${req.headers.origin}/?canceled=true`,
+        }
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
