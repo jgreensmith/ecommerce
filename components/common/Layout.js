80558ebe64a1621e000dd69a6217634998633982
James Greensmith <jgreensmith13@gmail.com>
2022-09-27 15:56:29 +0100

    layout preview working

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -6,172 +6,184 @@ import { Container, createTheme, CssBaseline, Toolbar, Typography } from '@mui/m
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Loader from '../svg/Loader';
+import { useRouter } from 'next/router';
+import { usePreviewSubscription } from '../../lib/sanity';
+import filterDataToSingleItem from '../../utils/functions';
 
 const Layout = ({ children, title, seo }) => {
+
+  const router = useRouter()
+  const preview = router.isPreview
+  const fetcher = (args) => fetch(args).then((res) => res.json())
+  const { data, error } = useSWR(['/api/sanity-settings', preview],fetcher )
+
+  const { data: previewSettings } = usePreviewSubscription(data?.query, {
+    initialData: data?.settings,
+    enabled: preview
+  })
+
+  const settings = filterDataToSingleItem(previewSettings, preview)
+
+  if (error) return <div>Failed to load</div>
+  //const data = null
+  if (!data) return <Loader/>
+
+
+  console.log(settings)
+  console.log(data)
+  //themes chosen from default palette
+  
+  const companyName = settings?.title;
+  const customColors = settings?.colorThemes;
+  const chosenPalette = JSON.parse(settings?.defaultThemes);
+
+  const contrastText = (h, dark, light) => {
+    //convert hex to rgb
+    let r = 0, g = 0, b = 0;
     
+    if(!h) {
+      return;
+    }
+      // 3 digits
+    if (h.length == 4) {
+      r = "0x" + h[1] + h[1];
+      g = "0x" + h[2] + h[2];
+      b = "0x" + h[3] + h[3];
+
+    // 6 digits
+    } else if (h.length == 7) {
+      r = "0x" + h[1] + h[2];
+      g = "0x" + h[3] + h[4];
+      b = "0x" + h[5] + h[6];
+    } 
+    //choose dark or light based on brightness of rgb
+    if ((r*0.299 + g*0.587 + b*0.114) > 186) {
+      return dark;
+    } else {
+      return light;
+    }
+
     
-    const fetcher = (...args) => fetch(...args).then((res) => res.json())
-    const { data, error } = useSWR('/api/sanity-settings', fetcher)
+  };
+  
 
-    if (error) return <div>Failed to load</div>
-    if (!data) return <Loader/>
+  const defaultTheme = {
+    primary: {
+      main: "#d4c3e9",
+      text: "#000000"
+    },
+    secondary: {
+      main: "#28c3d1",
+      text: '#000000'
+    },
+    error: {
+      main: '#f04000',
+    },
+    background: {
+      default: "#f1f3fa",
+      text: "#000000"
+    },
+    text: {
+      dark: "#000000",
+      light: "#ffffff"  
+    }
+  }
+  const muiChosenPalette = {
+    primary: {
+      main: chosenPalette?.primary,
+      text: contrastText(chosenPalette?.primary, chosenPalette?.dark, chosenPalette?.light)
+    },
+    secondary: {
+      main: chosenPalette?.secondary,
+      text: contrastText(chosenPalette?.secondary, chosenPalette?.dark, chosenPalette?.light)
+    },
+    error: {
+      main: '#f04000'
+    },
+    background: {
+      default: chosenPalette?.background,
+      text: contrastText(chosenPalette?.background, chosenPalette?.dark, chosenPalette?.light)
+    },
+    text: {
+      light: chosenPalette?.light,
+      dark: chosenPalette?.dark
+    }
+  }
 
-
-    //console.log(settings)
-    console.log(error)
-    //themes chosen from default palette
-    
-    const companyName = data[0]?.title;
-    const customColors = data[0]?.colorThemes;
-    const chosenPalette = JSON.parse(data[0]?.defaultThemes);
-
-    const contrastText = (h, dark, light) => {
-      //convert hex to rgb
-      let r = 0, g = 0, b = 0;
+  const muiCustomColors = {
+    primary: {
+      main: customColors?.primary,
+      text: contrastText(customColors?.primary, customColors?.dark, customColors?.light),
       
-      if(!h) {
-        return;
-      }
-        // 3 digits
-      if (h.length == 4) {
-        r = "0x" + h[1] + h[1];
-        g = "0x" + h[2] + h[2];
-        b = "0x" + h[3] + h[3];
+    },
+    secondary: {
+      main: customColors?.secondary,
+      text: contrastText(customColors?.secondary, customColors?.dark, customColors?.light)
+    },
+    error: {
+      main: '#f04000',
+    },
+    background: {
+      default: customColors?.background,
+      text: contrastText(customColors?.background, customColors?.dark, customColors?.light)
+    },
+    text: {
+      light: customColors?.light,
+      dark: customColors?.dark
+    }
+  }
+  
 
-      // 6 digits
-      } else if (h.length == 7) {
-        r = "0x" + h[1] + h[2];
-        g = "0x" + h[3] + h[4];
-        b = "0x" + h[5] + h[6];
-      } 
-      //choose dark or light based on brightness of rgb
-      if ((r*0.299 + g*0.587 + b*0.114) > 186) {
-        return dark;
+  const colorSorter = () => {
+
+
+      if(!customColors && chosenPalette) {
+        console.log('muiChosenPalette');
+        return muiChosenPalette;
+      
+      } else if (customColors) {
+        console.log('muiCustomPalette');
+        return muiCustomColors;
       } else {
-        return light;
-      }
+        console.log('defaultTheme');
+        return defaultTheme;
 
-      
-    };
+      }
     
+  }
 
-    const defaultTheme = {
-      primary: {
-        main: "#d4c3e9",
-        text: "#000000"
+  const sanityTheme = createTheme({
+      typography: {
+        h1: {
+          fontSize: '2.2rem',
+          fontWeight: 400,
+          margin: '2rem 0',
+        },
+        h2: {
+          fontSize: '1.8rem',
+          fontWeight: 400,
+          margin: '1rem 0',
+        },
+        h3: {
+          fontSize: '1.4rem',
+          fontWeight: 400,
+          margin: '1rem 0',
+        },
       },
-      secondary: {
-        main: "#28c3d1",
-        text: '#000000'
-      },
-      error: {
-        main: '#f04000',
-      },
-      background: {
-        default: "#f1f3fa",
-        text: "#000000"
-      },
-      text: {
-        dark: "#000000",
-        light: "#ffffff"  
-      }
-    }
-    const muiChosenPalette = {
-      primary: {
-        main: chosenPalette?.primary,
-        text: contrastText(chosenPalette?.primary, chosenPalette?.dark, chosenPalette?.light)
-      },
-      secondary: {
-        main: chosenPalette?.secondary,
-        text: contrastText(chosenPalette?.secondary, chosenPalette?.dark, chosenPalette?.light)
-      },
-      error: {
-        main: '#f04000'
-      },
-      background: {
-        default: chosenPalette?.background,
-        text: contrastText(chosenPalette?.background, chosenPalette?.dark, chosenPalette?.light)
-      },
-      text: {
-        light: chosenPalette?.light,
-        dark: chosenPalette?.dark
-      }
-    }
+      palette: colorSorter(),
 
-    const muiCustomColors = {
-      primary: {
-        main: customColors?.primary,
-        text: contrastText(customColors?.primary, customColors?.dark, customColors?.light),
-        
+      breakpoints: {
+        values: {
+          xs: 0,
+          vs: 500,
+          sm: 680,
+          md: 920,
+          lg: 1200,
+          xl: 1536,
+        },
       },
-      secondary: {
-        main: customColors?.secondary,
-        text: contrastText(customColors?.secondary, customColors?.dark, customColors?.light)
-      },
-      error: {
-        main: '#f04000',
-      },
-      background: {
-        default: customColors?.background,
-        text: contrastText(customColors?.background, customColors?.dark, customColors?.light)
-      },
-      text: {
-        light: customColors?.light,
-        dark: customColors?.dark
-      }
-    }
-   
-
-    const colorSorter = () => {
-
-
-        if(!customColors && chosenPalette) {
-          console.log('muiChosenPalette');
-          return muiChosenPalette;
-        
-        } else if (customColors) {
-          console.log('muiCustomPalette');
-          return muiCustomColors;
-        } else {
-          console.log('defaultTheme');
-          return defaultTheme;
-
-        }
       
-    }
-
-    const sanityTheme = createTheme({
-        typography: {
-          h1: {
-            fontSize: '2.2rem',
-            fontWeight: 400,
-            margin: '2rem 0',
-          },
-          h2: {
-            fontSize: '1.8rem',
-            fontWeight: 400,
-            margin: '1rem 0',
-          },
-          h3: {
-            fontSize: '1.4rem',
-            fontWeight: 400,
-            margin: '1rem 0',
-          },
-        },
-        palette: colorSorter(),
- 
-        breakpoints: {
-          values: {
-            xs: 0,
-            vs: 500,
-            sm: 680,
-            md: 920,
-            lg: 1200,
-            xl: 1536,
-          },
-        },
-       
-      });
+    });
 
     
 
@@ -194,11 +206,11 @@ const Layout = ({ children, title, seo }) => {
 
                 <CssBaseline />
 
-                <Navbar settings={data[0]} />
+                <Navbar settings={settings} />
                 <Container maxWidth="100%" disableGutters={true} sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }} >
                     <Toolbar />
                         {children}
-                    <Footer settings={data[0]} />
+                    <Footer settings={settings} />
                 </Container >
             </ThemeProvider>
 
--- pages/api/sanity-settings.js
+++ pages/api/sanity-settings.js
@@ -1,12 +1,19 @@
 import { groq } from "next-sanity"
-import { sanityClient } from "../../lib/sanity.server"
+import { useRouter } from "next/router"
+import { getClient, sanityClient } from "../../lib/sanity.server"
+import filterDataToSingleItem from "../../utils/functions"
 
 export default async function sanitySettings(req, res) {
+    
     try {
+        const { preview } = req
         const query =`*[_type == "siteSettings"]`
-        const data = await sanityClient.fetch(query)
+        const data = await getClient(preview).fetch(query)
 
-        res.status(200).json(data)
+        const settings = filterDataToSingleItem(data, preview)
+
+
+        res.status(200).json({ settings, query})
 
     } catch (error) {
         res.status(500).json({error: 'Something went wrong'})
