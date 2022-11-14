f8e92abd6d5f1c36e3f79deae2761eedfb672896
James Greensmith <jgreensmith13@gmail.com>
2022-11-14 12:54:50 +0000

    preview mode with pids

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,7 +1,7 @@
 export default function exit(_, res) {
     res.clearPreviewData()
   
-    res.writeHead(307, {Location: `/`})
+    res.writeHead(307, {Location: `/merchants/2uh6xbh5`})
     res.end()
 
   }
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,3 +1,4 @@
+import clientPromise from "../../lib/mongodb"
 import { previewClient } from "../../lib/sanity.server"
 
 function redirectToPreview(res, Location) {
@@ -15,17 +16,23 @@ export default async function preview(req, res) {
   
     // Check the secret and next parameters
     // This secret should only be known to this API route and the CMS
-    if (req.query.secret !== process.env.NEXT_PUBLIC_SANITY_PREVIEW_SECRET) {
-      return res.status(401).json({message: 'Invalid secret token'})
-    }
+    const client = await clientPromise
+     
+    const pidObj = await client.db('test').collection('users').findOne(
+        {preview_mode : { $eq: req.query.secret }},
+        {projection: {pid: 1, preview_mode: 1, _id: 0}}
+      )       
+    // if (req.query.secret !== "skpQhwhL8a9CIEz8vLuPmnnwSgLlB2WQGeHbAzCBFR61z8UolZjGsSdtmMJUjqQ3aoIDki1oicmqoJg3M1yWPfW0ZvtVA6bykm3mQBNWUJHVSX2aAbkjbRu1cAIKiNK0EwDozDjcJtLHaQHwlZse8nkmN0uCoabXro9D4NK0RCLJSxgCEWke") {
+    //   return res.status(401).json({message: 'Invalid secret token'})
+    // }
   
     if (!req.query.slug) {
-      return redirectToPreview(res, '/') 
+      return redirectToPreview(res, `/merchants/${pidObj.pid}`) 
     }
     
     //check if slug exists
     const productQuery = `*[_type == "product" && slug.current == $slug][0]`
-    const product = await previewClient.fetch(productQuery, {
+    const product = await previewClient(pidObj.pid).fetch(productQuery, {
       slug: req.query.slug
     })
 
@@ -35,5 +42,5 @@ export default async function preview(req, res) {
   
      // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  redirectToPreview(res, `/products/${req?.query?.slug}`)
+  redirectToPreview(res, `/merchants/${pidObj.pid}/products/${req?.query?.slug}`)
 }
